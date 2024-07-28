05e952decb82fbca0a7ee10e3c718f185c16c368
j-double-u <wangjohnandrew@gmail.com>
2024-07-28 19:22:50 -0400

    completed gameView class

--- src/frontend/gameView.js
+++ src/frontend/gameView.js
@@ -2,5 +2,29 @@ export class gameView {
     constructor() {}
 
     render() {
+        const gameViewElm = document.createElement('div');
+        gameViewElm = 'game-view';
+
+        const titleElm = document.createElement('h1');
+        titleElm.innerText = 'Game View';
+
+        const gameContainerElm = document.createElement('div');
+        gameContainerElm.id = 'game-container';
+
+        gameViewElm.appendChild(titleElm);
+        gameViewElm.appendChild(gameContainerElm);
+
+        const gameInfo = new gameInfo();
+        gameContainerElm.appendChild(gameInfo.render());
+
+        return gameViewElm;
+    }
+}
+
+class gameInfo {
+    constructor() {}
+
+    render() {
+
     }
 }
