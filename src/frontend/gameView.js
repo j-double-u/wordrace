aab7171142aafe9c9e0a0acbd17eaf659f8be40f
j-double-u <wangjohnandrew@gmail.com>
2024-07-29 00:09:44 -0400

    completed homeView (implemented word, definition list, and definition classes)

--- src/frontend/gameView.js
+++ src/frontend/gameView.js
@@ -1,3 +1,4 @@
+import { dictionary, keys } from "./dictionary.js";
 import { Events } from "./events.js";
 
 export class GameView {
@@ -34,12 +35,13 @@ class GameInfo {
         const word = new Word();
         const definitionList = new DefinitionList();
         const timerElm = timer.render();
-        const wordElm = word.render();
         const definitionListElm = definitionList.render();
+        const wordElm = word.render();
+        
 
         gameInfoElm.appendChild(timerElm);
-        // gameInfoElm.appendChild(wordElm);
-        // gameInfoElm.appendChild(definitionListElm);
+        gameInfoElm.appendChild(wordElm);
+        gameInfoElm.appendChild(definitionListElm);
         
         return gameInfoElm;
     }
@@ -73,17 +75,97 @@ class Timer {
 }
 
 class Word {
-    constructor() {}
+    #events = null;
+    #numProblems = 0;
+    #numCorrect = 0;
+    #wordElm = null;
+    
+    constructor() {
+        this.#events = Events.events();
+    }
 
     render() {
 
+        this.#numProblems++;
+        let wordNum = -1;
+        if (this.#numProblems === 1) {
+            this.#wordElm = document.createElement('p');
+            this.#wordElm.id = 'word';
+            // needs to change when person clicks on def (need to use this.render()???)
+            this.#events.subscribe('def-clicked', answeredNum => {
+                if (answeredNum === wordNum) {
+                    this.#numCorrect++;
+                }  
+                this.render();
+            });
+        }
+
+        if (this.#numProblems > 10) {
+            //publish when done with ten problems
+            this.#events.publish('game-over', this.#numCorrect);
+        }
+        else {
+            wordNum = Math.floor(Math.random() * keys.length);
+            this.#wordElm.innerText = keys[wordNum];
+
+            // Give the definition list the word
+            this.#events.publish('word-generated', wordNum);
+        }
+        return this.#wordElm;
     }
+
 }
 
 class DefinitionList {
-    constructor() {}
+    #events = null;
+    #tasks = [];
+    #list = null;
+
+    constructor() {
+        this.#events = Events.events();
+    }
 
     render() {
+        const definitionListElm = document.createElement('div');
+        definitionListElm.id = 'definition-list';
 
+        this.#list = document.createElement('ul');
+        definitionListElm.appendChild(this.#list);
+
+        this.#events.subscribe('word-generated', wordNum => {
+            this.#tasks = [];
+            this.#list.innerHTML = "";
+            
+            const correctNum = wordNum;
+            this.#tasks.push(correctNum);
+            for (let i = 0; i < 3; i++) {
+                this.#tasks.push(Math.floor(Math.random() * keys.length))
+            }
+            this.#tasks.sort();
+            const definitionElements = this.#tasks.map(wordNum => this.#makeDefinitionElement(wordNum));
+
+            definitionElements.forEach(element => this.#list.appendChild(element));
+
+        });
+
+        return definitionListElm;
     }
+
+    #makeDefinitionElement(wordNum) {
+        const definitionElm = document.createElement('button');
+        definitionElm.classList.add('definition');
+        definitionElm.innerText = dictionary[keys[wordNum]];
+
+
+        definitionElm.addEventListener('click', () => {
+            //let word know to change the word
+            //let word list know to recount the correct answers
+            this.#events.publish('def-clicked', wordNum);
+
+        });
+
+        return definitionElm;
+    }
+
+
 }
