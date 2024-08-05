bb872d7ec0c347b1e96f0670da498feff624a04d
j-double-u <wangjohnandrew@gmail.com>
2024-08-05 14:02:42 -0400

    created CRUD operations for profiles in crud.js

---
+++ src/backend/crud.js
@@ -0,0 +1,48 @@
+import PouchDB from 'pouchdb';
+
+const database = new PouchDB('db');
+
+export async function createProfile(username, password) {
+    const profile = { _id: username, password: password, highScore: 0};
+    try {
+        const createProfile = await database.put(profile);
+        return profile;
+    }
+    catch (err) {
+        return undefined;
+    }
+}
+
+export async function readProfile(username) {
+    try {
+        const readProfile = await database.get(username);
+        return readProfile;
+    }
+    catch (err) {
+        return undefined;
+    }
+}
+
+export async function updateProfile(username, password) {
+    try {
+        const readProfile = await database.get(username);
+        readProfile['password'] = password;
+        const updateProfile = await database.put(readProfile);
+        return updateProfile;
+    }
+    catch (err) {
+        return undefined;
+    }
+}
+
+export async function deleteProfile(username) {
+    try {
+        const readProfile = await database.get(username);
+        const deleteProfile = await database.remove(readProfile);
+        return deleteProfile;
+    }
+    catch (err) {
+        return undefined;
+    }
+}
+
